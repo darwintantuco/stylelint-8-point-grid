f7d83a61c199c12d41a9858ca3fe0d0b8abb7e4f
darwin christopher tantuco <darwinxciii@yahoo.com>
2018-10-02 02:16:32 +0800

    Refactor module.exports

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const stylelint = require("stylelint");
 const { createPlugin } = stylelint;
 const { ruleMessages, validateOptions, report } = stylelint.utils;
 
+const rules = {
+  "plugin/8-point-grid": {
+    base: 8
+  }
+};
+const blacklist = ["margin", "padding", "height", "width"];
+const plugins = ["stylelint-8-point-grid"];
 const ruleName = "plugin/8-point-grid";
 const messages = ruleMessages(ruleName, {
   invalid: (prop, actual, base) =>
@@ -13,41 +20,39 @@ const validBase = option => !isNaN(parseFloat(option)) && isFinite(option);
 const validWhitelist = option => hasPixelValue(option);
 
 const validPixelValue = (value, base, whitelist) => {
-  if (/\s/.test(value)) {
-    // Handles multiple px values
+  return value
+    // Handle multiple px values
     // e.g. padding: 8px 8px 1px 8px
-    values = value.split(/ +/);
-    return values.every(
-      value => isWhitelist(whitelist, value) || divisibleByBase(value, base)
+    .split(/ +/)
+    .every(
+      value => isWhitelist(whitelist, value) || divisibleBy(value, base)
     );
-  }
-  return isWhitelist(whitelist, value) || divisibleByBase(value, base);
 };
 
 const hasPixelValue = value => String(value).includes("px");
 const isWhitelist = (whitelist, value) =>
   (whitelist && whitelist.includes(value)) || false;
 
-const divisibleByBase = (value, base) => {
+const divisibleBy = (value, base) => {
   const number = value.match(/\d+/);
   if (number === null) return false;
   return Number(number) % Number(base) === 0;
 };
 
-module.exports = createPlugin(ruleName, (primaryOption, secondaryOption) => {
+const rule = createPlugin(ruleName, (primaryOption, secondaryOption) => {
   return (postcssRoot, postcssResult) => {
     const validOptions = validateOptions(postcssResult, ruleName, {
       actual: primaryOption,
       possible: {
         base: validBase,
-        ignore: ["margin", "padding", "height", "width"],
+        ignore: blacklist,
         whitelist: validWhitelist
       }
     });
     if (!validOptions) return;
 
+    let props = blacklist;
     const { ignore, whitelist } = primaryOption;
-    let props = ["margin", "padding", "height", "width"];
     if (ignore) props = props.filter(prop => !ignore.includes(prop));
 
     postcssRoot.walkDecls(pattern(props), decl => {
@@ -64,11 +69,4 @@ module.exports = createPlugin(ruleName, (primaryOption, secondaryOption) => {
   };
 });
 
-module.exports.ruleName = ruleName;
-module.exports.messages = messages;
-module.exports.plugins = ["stylelint-8-point-grid"];
-module.exports.rules = {
-  "plugin/8-point-grid": {
-    base: 8
-  }
-};
+module.exports = { ...rule, plugins, messages, rules };
