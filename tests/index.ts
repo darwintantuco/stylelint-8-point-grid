66db9e81ecde0687f3ad1c7a7fd07fda1b008c1f
darwin christopher tantuco <dcrtantuco@gmail.com>
2020-04-16 23:45:46 +0800

    Add node-ts to run tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,9 +1,11 @@
-const testRule = require('stylelint-test-rule-tape')
-const eightPointGrid = require('./dist/index')
+// @ts-ignore
+import testRule from 'stylelint-test-rule-tape'
+
+import { rule, ruleName, messages } from '../src/index'
 
 // base config
-testRule(eightPointGrid.rule, {
-  ruleName: eightPointGrid.ruleName,
+testRule(rule, {
+  ruleName: ruleName,
   config: {
     base: 8,
   },
@@ -41,50 +43,46 @@ testRule(eightPointGrid.rule, {
   reject: [
     {
       code: '.generic-card { margin: 2px 8px; }',
-      message: eightPointGrid.messages.invalid('margin', '2px 8px', 8),
+      message: messages.invalid('margin', '2px 8px', 8),
     },
     {
       code: '.generic-card { padding-left: 60px; }',
-      message: eightPointGrid.messages.invalid('padding-left', '60px', 8),
+      message: messages.invalid('padding-left', '60px', 8),
     },
     {
       code: '.generic-card { padding: 8px 8px   1px 8px; }',
-      message: eightPointGrid.messages.invalid(
-        'padding',
-        '8px 8px   1px 8px',
-        8
-      ),
+      message: messages.invalid('padding', '8px 8px   1px 8px', 8),
     },
     {
       code: '.generic-card { width: 100px; }',
-      message: eightPointGrid.messages.invalid('width', '100px', 8),
+      message: messages.invalid('width', '100px', 8),
     },
     {
       code: '.generic-card { margin-top: -60px; }',
-      message: eightPointGrid.messages.invalid('margin-top', '-60px', 8),
+      message: messages.invalid('margin-top', '-60px', 8),
     },
     {
       code: `.generic-card { margin: 1px\n  2px; }`,
-      message: eightPointGrid.messages.invalid('margin', '1px\n  2px', 8),
+      message: messages.invalid('margin', '1px\n  2px', 8),
     },
     {
       code: '.generic-card { top: 1px; }',
-      message: eightPointGrid.messages.invalid('top', '1px', 8),
+      message: messages.invalid('top', '1px', 8),
     },
     {
       code: '.generic-card { left: 31px; }',
-      message: eightPointGrid.messages.invalid('left', '31px', 8),
+      message: messages.invalid('left', '31px', 8),
     },
     {
       code: '.generic-card { padding-left: 8px; padding-top: 5px; }',
-      message: eightPointGrid.messages.invalid('padding-top', '5px', 8),
+      message: messages.invalid('padding-top', '5px', 8),
     },
   ],
 })
 
 // extended config
-testRule(eightPointGrid.rule, {
-  ruleName: eightPointGrid.ruleName,
+testRule(rule, {
+  ruleName: ruleName,
   config: {
     base: 4,
     whitelist: ['2px', '1px'],
@@ -107,19 +105,19 @@ testRule(eightPointGrid.rule, {
   reject: [
     {
       code: '.generic-card { padding-left: 3px; }',
-      message: eightPointGrid.messages.invalid('padding-left', '3px', 4),
+      message: messages.invalid('padding-left', '3px', 4),
     },
     {
       code: '.generic-card { height: 3px; }',
-      message: eightPointGrid.messages.invalid('height', '3px', 4),
+      message: messages.invalid('height', '3px', 4),
     },
     {
       code: '.generic-card { margin: 2px 11px; }',
-      message: eightPointGrid.messages.invalid('margin', '2px 11px', 4),
+      message: messages.invalid('margin', '2px 11px', 4),
     },
     {
       code: '.generic-card { margin: 1px 2px 3px 4px; }',
-      message: eightPointGrid.messages.invalid('margin', '1px 2px 3px 4px', 4),
+      message: messages.invalid('margin', '1px 2px 3px 4px', 4),
     },
   ],
 })
